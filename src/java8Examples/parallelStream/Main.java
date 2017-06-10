f34f1813ed2230ce27bfa2bfe322cd26bc43ae1a
Preeti Saha <preetisaha.a1@gmail.com>
2017-06-10 15:26:34 -0500

    parallel stream example

--- src/java8Examples/parallelStream/Main.java
+++ src/java8Examples/parallelStream/Main.java
@@ -1,4 +1,4 @@
-package java8Examples.stream;
+package java8Examples.parallelStream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,37 +10,36 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ArrayList<Integer> inputList = new ArrayList<>(Arrays.asList(4, 6, 7, 8, 9, 4, 2, 3, 4, 5, 6));
+ArrayList<Integer> inputList = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
 
 		
-		//map function enforces the lambda function provided over each value in a stream. 
-		//And returns a new stream containing the transformed values.
+		//parallel stream for map function enforces the lambda function provided over multiple values at a same time
+		//using thread in a stream. And returns a new stream containing the transformed values. Having println inside lambda
+		//function shows out of order due to multiple threads.
 		List<Integer> outputList = inputList.
-				stream().
+				parallelStream().
 				map(
-						(a) -> {
-							System.out.println("inside - " + a);
-							return a + 10;
+						(a) -> {System.out.println((a));
+							return a * 10;
 						}
 				).
 				collect(Collectors.toList());
 		
-		//filter function enforces the lambda function provided over each value in a stream and returns a boolean.
+		//filter function enforces the lambda function provided over multiple value in a stream and returns a boolean.
 		//The filter function returns a stream containing all the values for which the lambda function returns a true.
 		List<Integer> filterList = outputList.
-				stream().
+				parallelStream().
 				filter(
 						(x) -> {
 							return (x % 2 == 0);
 						}
 				).collect(Collectors.toList());
 		
-		
 		// reduce function enforce lambda function to take 2 input parameters.
 		// reduce function return a single entity after reducing the whole list.
-		Optional<Integer> totalSum = inputList.stream().reduce((i, j) -> {
+		Optional<Integer> totalSum = inputList.parallelStream().reduce((i, j) -> {
 			return i+j;
 		});
 	}
-}
 
+}
